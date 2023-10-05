da646bdfa518a9cf14763b7182fe5972869142ba
isabellacianorte <127401546+isabellacianorte@users.noreply.github.com>
2023-10-05 08:55:15 -0300

    Add files via upload

---
+++ java-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,18 @@
+function tocasompom () {
+    document.querySelector('som_tecla_pom').play();
+}
+
+document.querySelector(".tecla_pom").onclick = tocasompom;
+
+
+function tocasomclap () {
+    document.querySelector('som_tecla_clap').play();
+}
+
+document.querySelector(".tecla_tim").onclick = tocasomtim;
+
+function tocasomtim () {
+    document.querySelector('som_tecla_tim').play();
+}
+
+document.querySelector(".tecla_tim").onclick = tocasomtim;
